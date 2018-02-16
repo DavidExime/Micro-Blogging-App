68a9fd763ee0099b5b8f798c1e8dd6352ca2b9a6
davidexime <david.exime1@gmail.com>
2018-02-16 12:41:24 -0500

    updated folder structure

--- app.rb
+++ app.rb
@@ -5,20 +5,36 @@ require './models'
 set :database, "sqlite3:main.sqlite3"
 set :sessions, true
 
-
-get '/' do
-	@edit =  
-
-
 get '/' do 
 	@blogs = Blog.all
 erb :home	
 end
 
-get '/users-:id' do
-@user = User.find(params[:id])
-erb :'users/profile'
+get '/users/:id' do
+	@user = User.find(params[:id])
+	erb :'users/profile'
 end
+
+get '/edit' do
+	@user = User.find(params[:id])
+	erb :'users/edit' 
+
+end 
+
+get '/login' do
+	@user = User.find(params[:id])
+	erb :'users/login'
+end
+
+
+get '/signup' do
+	@user = User.find(params[:id])
+	erb :'users/signup'
+
+end
+
+
+
 
 
 
